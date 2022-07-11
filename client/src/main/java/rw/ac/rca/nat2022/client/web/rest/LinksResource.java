b87a64c66b472f21e826eb9f3b11b43ba8f7a675
oreste-abizera <oresteabizera11@gmail.com>
2022-07-11 10:37:50 +0200

    protect routes

--- client/src/main/java/rw/ac/rca/nat2022/client/web/rest/LinksResource.java
+++ client/src/main/java/rw/ac/rca/nat2022/client/web/rest/LinksResource.java
@@ -1,14 +1,20 @@
 package rw.ac.rca.nat2022.client.web.rest;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.servlet.http.HttpServletRequest;
 
 @Controller
 @RequestMapping("links")
 public class LinksResource {
     @GetMapping("list")
-    public String list() {
+    public String list(HttpServletRequest request, Model model) {
+        if(request.getSession().getAttribute("token") == null || request.getSession().getAttribute("token").toString().isEmpty()){
+            return "redirect:/auth/login";
+        }
         return "links/list";
     }
 }
--- client/src/main/java/rw/ac/rca/nat2022/client/web/rest/WebsitesResource.java
+++ client/src/main/java/rw/ac/rca/nat2022/client/web/rest/WebsitesResource.java
@@ -1,19 +1,28 @@
 package rw.ac.rca.nat2022.client.web.rest;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.servlet.http.HttpServletRequest;
 
 @Controller
 @RequestMapping("websites")
 public class WebsitesResource {
     @GetMapping("list")
-    public String list() {
+    public String list(HttpServletRequest request, Model model) {
+        if(request.getSession().getAttribute("token") == null || request.getSession().getAttribute("token").toString().isEmpty()){
+            return "redirect:/auth/login";
+        }
         return "websites/list";
     }
 
     @GetMapping("crawl")
-    public String crawl() {
+    public String crawl(HttpServletRequest request, Model model) {
+        if(request.getSession().getAttribute("token") == null || request.getSession().getAttribute("token").toString().isEmpty()){
+            return "redirect:/auth/login";
+        }
         return "websites/crawl";
     }
 }
